02c36505714e7d2a069246918806f7d8832187ec
YannisSchmutz <yannis.schmutz@gmail.com>
2019-10-27 19:06:36 +0100

    Add picture paths to users and projects as well as some example images

--- src/database/migrations/2019_10_20_101916_add_collabo_attributes_to_users_table.php
+++ src/database/migrations/2019_10_20_101916_add_collabo_attributes_to_users_table.php
@@ -28,7 +28,9 @@ class AddCollaboAttributesToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('surname');
+            $table->dropColumn('pitch');
+            $table->dropColumn('description');
         });
     }
 }
