32ce62d4ef0638fd842186bfc3c51ac5b96ebb4a
YannisSchmutz <yannisvalentin.schmutz@students.bfh.ch>
2019-11-03 19:23:27 +0100

    Update src/resources/lang/en/profiletext.php
    Co-Authored-By: Kevin Riesen <kevin.riesen@hotmail.com>

--- src/resources/lang/en/profiletext.php
+++ src/resources/lang/en/profiletext.php
@@ -11,7 +11,7 @@ return [
     'interests_title' => 'My Interests',
     'projectsts_title' => 'My Projects',
     'fullname_form' => 'User name:',
-    'caption_form' => 'Profil slogan:',
+    'caption_form' => 'Profile slogan:',
     'pitch_form' => 'Pitch:',
     'pic_form' => 'Profile Picture:',
 
